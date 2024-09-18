431a59383803fd8ad783353c560f110f2343ec20
GratG <Grantgroenewaldza@gmail.com>
2024-09-18 14:45:06 +0200

    csv saving enabled

--- lib/player.py
+++ lib/player.py
@@ -5,29 +5,39 @@ import json
 import pandas
 
 import numpy
-
-from datetime import datetime
 from win10toast import ToastNotifier
 
-class player:
-    def __init__(self, n):
-        print("player {} initiated".format(n))
-        self.name = n
-        self.data = call_player(n)
-
-        
-    
-def call_player(n):
-    
-    stats = ['Overall', 'attack', 'defence', 'strength', 'constitution', 
+CONST_STATS = ['Overall', 'attack', 'defence', 'strength', 'constitution', 
               'ranged', 'magic', 'cooking', 'woodcutting', 'fletching', 
               'fishing', 'firemaking', 'crafting', 'smithing', 'mining',
               'herblore', 'agility', 'thieving', 'slayer', 'farming',
               'runecrafting', 'hunter', 'construction', 'summoning',
               'dungeoneering', 'divination', 'invention', 'archaeology',
               'necromancy']
+class player:
+    def __init__(self, n):
+        print("player {} initiated".format(n))
+        self.data = call_player(n)
+        self.name = n
+        self.rankList = {}
+        self.levelList = {}
+        self.expList = {}
+        for s in CONST_STATS:
+            dataSet = self.data[s]
+            dataList = dataSet.split(",")
+            self.rankList[s] = dataList[0]
+            self.levelList[s] = dataList[1]
+            self.expList[s] = dataList[2]
+
+
+        
+    
+def call_player(n):
+    
               
     response = requests.get("https://secure.runescape.com/m=hiscore/index_lite.ws?player={}".format(n))
+    print("Response is type:",type(response.text))
+    
     if(response.ok):
         print("response ok")
     else:
@@ -38,12 +48,16 @@ def call_player(n):
     
     i = 0
     data = {}
-    for s in stats:
+    data['name'] = n
+    for s in CONST_STATS:
         data[s] = stat_list[i]
         i += 1
-    print(type(data))
-    jdata = json.dumps(data)
+    
+    
+    with open('data.json', 'w') as file:
+        json.dump(data, file)
     return data 
     
 
 
+#call_player("VarroMalleus")
--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import os, sys, discord
+import json
+import csv
+from datetime import datetime
 #sys.path.append('C:/Dev/RuneBot/lib')
 
 from lib.player import player
+from dotenv import load_dotenv
 
+CONST_STATS = ['Overall', 'attack', 'defence', 'strength', 'constitution', 
+              'ranged', 'magic', 'cooking', 'woodcutting', 'fletching', 
+              'fishing', 'firemaking', 'crafting', 'smithing', 'mining',
+              'herblore', 'agility', 'thieving', 'slayer', 'farming',
+              'runecrafting', 'hunter', 'construction', 'summoning',
+              'dungeoneering', 'divination', 'invention', 'archaeology',
+              'necromancy']
 
 def resourcePath(relativePath):
     try:
@@ -18,16 +29,57 @@ def ensure_dir():
     if not os.path.exists('csv'):
         os.makedirs('csv')
         
+def save_csv(p):
+    fieldnames = CONST_STATS
+    fieldnames.append('date/time')
+    print(fieldnames)
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    print(dt_string)
+    if not os.path.exists('csv/{}'.format(p.name)):
+        os.makedirs('csv/{}'.format(p.name))
+        with open('csv/{}/rank-values.csv'.format(p.name), 'w', newline='') as csvFile:
+            csvWriter = csv.DictWriter(csvFile, fieldnames=fieldnames)
+            csvWriter.writeheader()
+        with open('csv/{}/level-values.csv'.format(p.name), 'w', newline='') as csvFile:
+            csvWriter = csv.DictWriter(csvFile, fieldnames=fieldnames)
+            csvWriter.writeheader()
+        with open('csv/{}/exp-values.csv'.format(p.name), 'w', newline='') as csvFile:
+            csvWriter = csv.DictWriter(csvFile, fieldnames=fieldnames)
+            csvWriter.writeheader()
+    rankList = p.rankList
+    levelList = p.levelList
+    expList = p.expList
+
+    rankList['date/time'] = dt_string
+    levelList['date/time'] = dt_string
+    expList['date/time'] = dt_string
+
+
+    with open('csv/{}/rank-values.csv'.format(p.name), 'a', newline='') as csvFile:
+        csvWriter = csv.DictWriter(csvFile, fieldnames=fieldnames)
+        csvWriter.writerow(rankList)
+    with open('csv/{}/level-values.csv'.format(p.name), 'a', newline='') as csvFile:
+        csvWriter = csv.DictWriter(csvFile, fieldnames=fieldnames)
+        csvWriter.writerow(levelList)
+    with open('csv/{}/exp-values.csv'.format(p.name), 'a', newline='') as csvFile:
+        csvWriter = csv.DictWriter(csvFile, fieldnames=fieldnames)
+        csvWriter.writerow(expList)
+        
+
+    
+    
+        
+try:
+    p1 = player("VarroMalleus")
+except:
+    print("failed to create player")
+
+save_csv(p1)
+
+
 
 ensure_dir()
-
-p1 = player("VarroMalleus")
-print(type(p1))
-print(p1.data)
-
-from dotenv import load_dotenv
-
-
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
